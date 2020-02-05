5786fbed17dfa1d347e557b7b07690f7d7a38ec8
Taylor <thamilton162@west-mec.org>
2020-02-05 15:29:46 -0700

    almost 4

--- case3/ah_report.js
+++ case3/ah_report.js
@@ -29,8 +29,17 @@
 */
 
 var donationTotal=0;
+donors.forEach(calcSum);
 
+var summaryTable="<table><tr><th>Donors</th><td>"+donors.length+"</td></tr><tr><th>Total Donations</th><td>$"+donationTotal.toLocaleString()+"</td></tr></table>";
+document.getElementById("donationSummary").innerHTML=summaryTable;
+var majorDonors=donors.filter(findMajorDonors);
+majorDonors.sort(donorSortDescending);
 
+var donorTable="<table><caption>Major Donors</caption><tr><th>Donation></th><th>Donor ID</th><th>Date</th><th>Name</th><th>Address</th><th>Phone</th><th>E-mail</th></tr>";
+majorDonors.forEach(writeDonorRow);
+donorTable+="</table>";
+document.getElementById("donorTable").innerHTML=donorTable;
 
 
 
